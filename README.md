8be31a250c79e59c07e368cbb39688969e83be44
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2017-08-21 00:01:04 -0300

    :construction: Extracts access token

---
+++ README.md
@@ -0,0 +1,5 @@
+API:
+
+https://www.vr.com.br/api-web/beneficiario/cartao/listar
+header: access-token
+
--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const puppeteer = require('puppeteer');
     request.abort();
   }
   else {
-    console.log(request);
+    console.log(request.headers.get("access-token"));
     request.continue();
   }})
   await page.goto('https://www.vr.com.br/portal/portal-vr/login');
-  await page.focus(".vr-login-input")
-  await page.type('seu email');
+  await page.focus("input[name=login]")
+  await page.type('email');
   await page.focus("input[name=password]")
-  await page.type('sua senha');
+  await page.type('senha');
   let inputElement = await page.$('button[type=submit]');
   await inputElement.click();
   await page.waitForSelector('.tabs');
